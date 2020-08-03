f56f77180091f4f213ea38dae51376f718c2e4d9
Uzodimma <Charltonujic@rocketmail.com>
2020-08-03 20:49:22 +0000

    Move to a new instance

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=uzodimma/microservices_project:latest
+dockerimage=house-prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment $dockerimage --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl expose deployment $dockerimage --type=LoadBalancer --port=8080
 
+kubectl get services
